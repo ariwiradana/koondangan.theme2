21522c708eb48f383a61b60291ed5625f119d425
Ari Wiradana <ariwiradana5@gmail.com>
2023-09-15 03:23:18 +0800

    sdsd

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import "swiper/css";
 import "swiper/css/effect-fade";
 import "aos/dist/aos.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
@@ -12,6 +13,9 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
+      <Head>
+        <title>The Wedding of {process.env.NEXT_PUBLIC_NAME}</title>
+      </Head>
       <Component {...pageProps} />
     </>
   );
